06b7fc247913d83cb4a9ebb4fa2a63ecca92d771
Sandip Sadhukhan <sandippakhanna@gmail.com>
2025-03-25 17:57:20 +0530

    templates added

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
+def customers(request):
+    return render(request, 'dashboard/customers.html')
