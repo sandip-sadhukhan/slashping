5bdc9302952b9d956df77e06cc9c909c538d2ffa
Sandip Sadhukhan <sandippakhanna@gmail.com>
2025-03-26 11:20:29 +0530

    customers dashboard page done

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from app import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('customers/', views.customers, name='customers'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'home.html')
+
+# Intermediate route for going to dashboard
+@login_required
+def dashboard(request):
+    return redirect("customers")
 
 @login_required
 def customers(request):
