5a6935bf1d625a707e9a17ab82d093e6af188a0c
Sandip Sadhukhan <sandippakhanna@gmail.com>
2025-04-24 17:52:20 +0530

    fix static and media files to s3

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -4,17 +4,27 @@ INSTALLED_APPS  += [
     'storages'
 ]
 
-# AWS S3 settings
-# AWS_ACCESS_KEY_ID = 'your-access-key-id'
-# AWS_SECRET_ACCESS_KEY = 'your-secret-access-key'
-AWS_STORAGE_BUCKET_NAME = 'slashping-static-media'
-AWS_S3_REGION_NAME = 'ap-south-1'  # Match your bucket's region
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
-# Static files
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-
-# Media files
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+        "OPTIONS": {
+            "access_key": os.getenv("AWS_S3_ACCESS_KEY"),
+            "secret_key": os.getenv("AWS_S3_SECRET_KEY"),
+            "bucket_name": os.getenv("AWS_S3_BUCKET_NAME"),
+            "custom_domain": f"{os.getenv("AWS_S3_BUCKET_NAME")}.s3.amazonaws.com",
+            "file_overwrite": False,
+            "location": "media",
+        },
+    },
+    'staticfiles': {
+        "BACKEND": "storages.backends.s3.S3Storage",
+        "OPTIONS": {
+            "access_key": os.getenv("AWS_S3_ACCESS_KEY"),
+            "secret_key": os.getenv("AWS_S3_SECRET_KEY"),
+            "bucket_name": os.getenv("AWS_S3_BUCKET_NAME"),
+            "custom_domain": f"{os.getenv("AWS_S3_BUCKET_NAME")}.s3.amazonaws.com",
+            "file_overwrite": False,
+            "location": "static",
+        },
+    },
+}
